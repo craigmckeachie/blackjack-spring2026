c12bdd04e8d015a1d9a5036a11f6c06806032060
Craig McKeachie <craigdmckeachie@gmail.com>
2026-05-07 14:42:13 -0400

    Format Hand

--- src/main/java/com/prizepicks/blackjack/Hand.java
+++ src/main/java/com/prizepicks/blackjack/Hand.java
@@ -4,22 +4,26 @@ import java.util.ArrayList;
 
 public class Hand {
     private ArrayList<Card> cards;
-    public Hand(){
+
+    public Hand() {
         cards = new ArrayList<>();
     }
+
     // A Card is dealt to the Hand and the Hand is responsible
-// to store the card
-    public void deal(Card card){
+    // to store the card
+    public void deal(Card card) {
         cards.add(card);
     }
-    public int getSize(){
+
+    public int getSize() {
         return cards.size();
     }
+
     // The Hand uses the methods of each card to determine
-// the value of each card - and adds up all values
-    public int getValue(){
+    // the value of each card - and adds up all values
+    public int getValue() {
         int value = 0;
-        for(Card card: cards){
+        for (Card card : cards) {
             card.flip(); // turn the card over to see the value
             value += card.getPointValue();
             card.flip(); // hide the card again
